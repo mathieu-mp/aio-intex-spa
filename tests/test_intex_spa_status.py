16da8c957955003aa685355169a7ce58f28e3f4e
Mathieu Payrol <mathieu.payrol@gmail.com>
2022-06-21 18:27:07 +0000

    Parse error codes from status

--- tests/test_intex_spa_status.py
+++ tests/test_intex_spa_status.py
@@ -1,17 +1,35 @@
 from intex_spa.intex_spa_status import IntexSpaStatus
 
-status_str = "FFFF110F010700220000000080808022000012"
-status_int = int("0x" + status_str, 16)
-
 
 def test_intex_spa_status():
+    """Test function for IntexSpaStatus with standard input status."""
+    status_str = "FFFF110F010700220000000080808022000012"
+    status_int = int("0x" + status_str, 16)
     intex_spa_status = IntexSpaStatus(status_int)
     assert intex_spa_status.power
     assert intex_spa_status.filter
     assert intex_spa_status.heater
-    assert not intex_spa_status.jets
-    assert not intex_spa_status.bubbles
-    assert not intex_spa_status.sanitizer
+    assert intex_spa_status.jets is False
+    assert intex_spa_status.bubbles is False
+    assert intex_spa_status.sanitizer is False
     assert intex_spa_status.unit == "°C"
     assert intex_spa_status.current_temp == 34
+    assert intex_spa_status.error_code is False
+    assert intex_spa_status.preset_temp == 34
+
+
+def test_intex_spa_error_code():
+    """Test function for IntexSpaStatus with E81 error input status."""
+    status_str = "FFFF110F010700B50000000080808022000012"
+    status_int = int("0x" + status_str, 16)
+    intex_spa_status = IntexSpaStatus(status_int)
+    assert intex_spa_status.power
+    assert intex_spa_status.filter
+    assert intex_spa_status.heater
+    assert intex_spa_status.jets is False
+    assert intex_spa_status.bubbles is False
+    assert intex_spa_status.sanitizer is False
+    assert intex_spa_status.unit == "°C"
+    assert intex_spa_status.current_temp is False
+    assert intex_spa_status.error_code == "E81"
     assert intex_spa_status.preset_temp == 34
