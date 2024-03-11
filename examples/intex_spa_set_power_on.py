fc963803461dbec071afd30dd0657a4eb8e7b7cb
Mathieu <mathieu.payrol@gmail.com>
2024-03-11 20:31:06 +0000

    typo fixes

--- examples/intex_spa_set_power_on.py
+++ examples/intex_spa_set_power_on.py
@@ -1,4 +1,4 @@
-"""Usage example: Set spa power off"""
+"""Usage example: Set spa power on"""
 import os
 import logging
 import asyncio
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 async def set_spa_power_on():
-    """Set spa power off"""
+    """Set spa power on"""
     spa = IntexSpa(SPA_ADDRESS)
 
     print(await spa.async_set_power())
